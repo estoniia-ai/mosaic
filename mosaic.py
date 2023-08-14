c47824c14c79d9b2ae683e2126cc7739e0bd681e
Otto Mättas <ottomattas@gmail.com>
2023-08-14 22:33:18 +0300

    Beautify

--- mosaic.py
+++ mosaic.py
@@ -10,43 +10,28 @@ def resize_proportionally(image, target_size):
     else:
         new_height = target_size
         new_width = int(target_size * aspect)
-    image = image.resize((new_width, new_height))
-    return image
+    return image.resize((new_width, new_height))
 
-def get_target_pixels(image, target_image_pixels):
-    width, height = image.size
-    for x in range(0, width):
-        for y in range(0, height):
+def get_target_pixels(image):
+    target_image_pixels = []
+    for x in range(image.width):
+        for y in range(image.height):
             pixel = image.getpixel((x,y))
-            if len(pixel) == 4:
-                r, g, b, a = pixel
-            else:
-                r, g, b = pixel
-            average = int((r+g+b)/3)
+            r, g, b = pixel[:3]
+            average = int((r + g + b) / 3)
             target_image_pixels.append(average)
     return target_image_pixels
 
-def get_source_averages(path, image_list, image_brightness_list, source_image_size):
+def get_source_averages(path, source_image_size):
+    image_list = []
+    image_brightness_list = []
+    
     for file in os.listdir(path):
-        source_image = Image.open("{}/{}".format(path, file))
-        resized_source_image = resize_crop(source_image, source_image_size)
+        source_image = Image.open(os.path.join(path, file)).convert('RGBA')
+        resized_source_image = resize_proportionally(source_image, source_image_size)
         image_list.append(resized_source_image)
-
-    for image in image_list:
-        width, height = image.size
-        r_total, g_total, b_total, count = 0, 0, 0, 0
-        for x in range(width):
-            for y in range(height):
-                pixel = image.getpixel((x, y))
-                if len(pixel) == 4:
-                    r, g, b, a = pixel
-                else:
-                    r, g, b = pixel
-                r_total += r
-                g_total += g
-                b_total += b
-                count += 1
-        average_brightness = int((((r_total + g_total + b_total) / count) / 3))
+        total_brightness = sum(resized_source_image.resize((1, 1)).getpixel((0, 0))[:3])
+        average_brightness = total_brightness // 3
         image_brightness_list.append(average_brightness)
 
     return image_list, image_brightness_list
@@ -55,14 +40,9 @@ def get_choices(target_image_pixels, image_list, image_brightness_list):
     choice_list = []
     threshold = 40
     for pixel in target_image_pixels:
-        possible_matches = []
-        for b in image_brightness_list:
-            if abs(b - pixel) <= threshold:
-                possible_matches.append(image_list[image_brightness_list.index(b)])
-        
+        possible_matches = [img for img, b in zip(image_list, image_brightness_list) if abs(b - pixel) <= threshold]
         if not possible_matches:
-            possible_matches.append(random.choice(image_list))
-            print("Added a random choice!")
+            possible_matches = image_list
         choice_list.append(random.choice(possible_matches))
     return choice_list
 
@@ -75,39 +55,26 @@ def stitch(new_image, choice_list, source_image_size):
 
 def main():
     target_image_path = input("Enter the path to the target image: ")
-    source_image_folder = input("Enter the path to the source images folder (folder should contain between 400-1,000 images for best results): ")
-    final_size = int(input("Enter target height of final image (pixel values between 1,000-20,000 for best results): "))
-    source_image_size = int(input("Enter the size of source images (pixel values between 50-200 for best results): "))
+    source_image_folder = input("Enter the path to the source images folder: ")
+    final_size = int(input("Enter target height of final image: "))
+    source_image_size = int(input("Enter the size of source images: "))
     
-    image_list = []
-    image_brightness_list = []
-
     target_image = Image.open(target_image_path).convert('RGBA')
     target_image_resized = resize_proportionally(target_image, final_size)
 
-    # Create the new image
-    new_image = Image.new('RGBA', (target_image_resized.width, target_image_resized.height))
-    
-    target_image_pixels = []
+    new_image = Image.new('RGBA', target_image_resized.size)
 
     print("Getting pixel values from target image...")
-    target_image_pixels = get_target_pixels(target_image_resized, target_image_pixels)
+    target_image_pixels = get_target_pixels(target_image_resized)
 
     print("Resizing and gathering pixel data from source images...")
-    image_list, image_brightness_list = get_source_averages(source_image_folder, image_list, image_brightness_list, source_image_size)
+    image_list, image_brightness_list = get_source_averages(source_image_folder, source_image_size)
 
     print("Calculating matches for pixels...")
     choice_list = get_choices(target_image_pixels, image_list, image_brightness_list)
 
     print("Stitching images into final output image...")
     stitch(new_image, choice_list, source_image_size)
-
-    # Ensure the images have the same dimensions
-    print(target_image_resized.size, target_image_resized.mode)
-    print(new_image.size, new_image.mode)
-
-    if target_image_resized.size != new_image.size:
-        new_image = new_image.resize(target_image_resized.size)
 
     # Blend the images
     final_image = Image.blend(target_image_resized, new_image, .65)
